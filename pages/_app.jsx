2daf51fd236beb900a0008ff30555229eef82190
Tasnimul-Sharan <tasnimulsharan8962@gmail.com>
2026-06-15 23:14:42 +0600

    updtaed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
         strategy="afterInteractive"
       />
       {/* <EventPopup /> */}
-      <OfferPopup />
+      {/* <OfferPopup /> */}
       <Component {...pageProps} />
       <Analytics />
     </Layout>
